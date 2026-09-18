e790c37d5ff66be1569ec1d10fecf3be11a9f1a3
aliganey2016000-del <aliganey2016000@gmail.com>
2026-09-18 17:37:25 +0300

    Allow optional password change when editing nomination

--- frontend/src/services/admin.service.ts
+++ frontend/src/services/admin.service.ts
@@ -298,6 +298,7 @@ export class AdminApiService {
     fullName: string;
     studentNumber: string;
     email: string;
+    password?: string;
     phone?: string;
     program?: string;
     specialty?: string;
